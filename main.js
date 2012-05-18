ed9461ede512525264a13d4abe3076b44c8d24d5
Lance Ennen <byennen@gmail.com>
2012-05-18 14:47:18 -0500

    adding two more photos

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){	   
 	$('BODY').bgStretcher({
-		images: ['images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg', 'images/5.jpg', 'images/6.jpg', 'images/7.jpg'],
+		images: ['images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg', 'images/5.jpg', 'images/6.jpg', 'images/7.jpg', 'images/8.jpg', 'images/9.jpg'],
 		imageWidth: 1024, 
 		imageHeight: 768, 
 		slideDirection: 'N',
